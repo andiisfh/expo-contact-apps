6e17eabd971e0413062079ab38c1fe82e3234f4f
Andi Insanudin <andi.insanudin@btpn.com>
2019-09-28 07:22:04 +0700

    Display json response to Flatlist

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, Image } from 'react-native';
 import axios from 'axios';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+    }
+  }
 
   componentDidMount(){
     this.getData();
@@ -10,24 +17,36 @@ export default class App extends React.Component {
 
   getData() {
     axios.get('https://simple-contact-crud.herokuapp.com/contact')
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
+    .then(response => {
+      this.setState({
+        dataSource: response.data.data,
+      });
     })
     .catch(function (error) {
       // handle error
       console.log(error);
-    })
+    });
   }
+
+  renderViewItem = (item) => (
+    <View style={{flex: 1, padding:20, flexDirection: 'row'}}>
+      <Image
+        style={{width: 50, height: 50, marginRight: 15}}
+        source={{uri: item.photo}}
+      />
+      <View>
+        <Text>{item.firstName} {item.lastName}</Text>
+        <Text>{item.age} years old</Text>
+      </View>
+   </View>
+  );
   
-render() {
-    const dummy = ['Andi', 'Luthfi', 'Leo', 'Pandu'];
-    
+  render() {
     return (
       <View style={{flex: 1, paddingTop:20}}>
         <FlatList
-          data={dummy}
-          renderItem={({item}) => <View style={{flex: 1, padding:20}}><Text>{item}</Text></View>}
+          data={this.state.dataSource}
+          renderItem={({item}) => this.renderViewItem(item)}
           keyExtractor={({item}, index) => index.toString()}
         />
       </View>
