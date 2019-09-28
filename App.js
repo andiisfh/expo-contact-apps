20ede2d1eccc41687aca13f37a2af8dcb7d7ec8c
Andi Insanudin <andi.insanudin@btpn.com>
2019-09-28 07:16:24 +0700

    Implement axios and log the response

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+export default class App extends React.Component {
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  getData() {
+    axios.get('https://simple-contact-crud.herokuapp.com/contact')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }
+  
+  render() {
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
+    
+    return (
+      <View style={styles.container}>
+      <Text>Hello world!</Text>
     </View>
-  );
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
