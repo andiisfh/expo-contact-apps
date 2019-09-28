c75fc8b487dd1f9d73bae826d5822a7e07361494
Andi Insanudin <andi.insanudin@btpn.com>
2019-09-28 08:45:17 +0700

    install react navigation & define stack navigator

--- App.js
+++ App.js
@@ -1,64 +1,15 @@
-import React from 'react';
-import { Text, View, FlatList, Image, Button } from 'react-native';
-import axios from 'axios';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import ListContactPage from './ListContactPage';
+import DetailContactPage from './DetailContactPage';
+import PostContactPage from './PostContactPage';
 
-export default class App extends React.Component {
+const MainNavigator = createStackNavigator({
+  ListContact: {screen: ListContactPage},
+  DetailContact: {screen: DetailContactPage},
+  PostContact: {screen: PostContactPage},
+});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: [],
-    }
-  }
+const App = createAppContainer(MainNavigator);
 
-  componentDidMount(){
-    this.getData();
-  }
-
-  getData() {
-    axios.get('https://simple-contact-crud.herokuapp.com/contact')
-    .then(response => {
-      this.setState({
-        dataSource: response.data.data,
-      });
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-  }
-
-  renderViewItem = (item) => (
-    <View style={{flex: 1, padding:20, flexDirection: 'row'}}>
-      <Image
-        style={{width: 50, height: 50, marginRight: 15}}
-        source={{uri: item.photo}}
-      />
-      <View>
-        <Text>{item.firstName} {item.lastName}</Text>
-        <Text>{item.age} years old</Text>
-      </View>
-   </View>
-  );
-  
-  addContact = () => {
-    const dummyContact = { 'firstName': 'Andi', 'lastName': 'Insanudin', 'age': '18', 'photo': 'N/A' };
-
-    this.setState(prevState => ({
-      dataSource: [...prevState.dataSource, dummyContact]
-    }))
-  }
-
-  render() {
-    return (
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => this.renderViewItem(item)}
-          keyExtractor={({item}, index) => index.toString()}
-        />
-        <Button title='Add Contact' onPress={()=> this.addContact()}/>
-      </View>
-    );
-  }
-}
+export default App;
