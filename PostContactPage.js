6e3eaa74436386627968fc36ad267194c0721393
Andi Insanudin <andi.insanudin@btpn.com>
2019-09-28 07:39:36 +0700

    Implement storing data

--- PostContactPage.js
+++ PostContactPage.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 export default class PostContactPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      age: 0,
+      photo: ''
+    }
+  }
+    
+  handleSubmit() {
+    axios.post('https://simple-contact-crud.herokuapp.com/contact', {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        age: this.state.age,
+        photo: this.state.photo
+    })
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+  }
 
   render() {
     const styles = StyleSheet.create({
@@ -18,22 +46,26 @@ export default class PostContactPage extends React.Component {
       <View style={{flex: 1, paddingTop:20, paddingHorizontal: 20}}>
         <TextInput 
           placeholder='First Name'
+          onChangeText={(value) => this.setState({firstName: value})}
           style={styles.inputStyle}
         />
         <TextInput 
           placeholder='Last Name'
+          onChangeText={(value) => this.setState({lastName: value})}
           style={styles.inputStyle}
         />
         <TextInput 
           placeholder='Age'
           keyboardType={"numeric"}
+          onChangeText={(value) => this.setState({age: value})}
           style={styles.inputStyle}
         />
         <TextInput 
           placeholder='Photo URL'
+          onChangeText={(value) => this.setState({photo: value})}
           style={styles.inputStyle}
         />
-        <Button title='Save'/>
+        <Button title='Save' onPress={() => this.handleSubmit()}/>
       </View>
     );
   }
